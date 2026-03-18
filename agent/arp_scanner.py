105d3ad256521af261f445a30d71cecaae87643b
Sir-em <meristiffany@gmail.com>
2026-03-18 22:11:58 +0530

    Update arp_scanner.py

--- agent/arp_scanner.py
+++ agent/arp_scanner.py
@@ -1,21 +1,32 @@
-from scapy.all import ARP, Ether, srp, conf
+from scapy.all import ARP, Ether, srp
 
-def scan_network(ip_range="192.168.1.0/24"):
-    conf.iface = "Wi-Fi"   # or "Ethernet" (check your adapter name)
+def scan_network(network):
+    """
+    Performs an ARP scan on the specified network.
 
-    arp = ARP(pdst=ip_range)
-    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    Args:
+        network (str): Example '192.168.1.0/24'
 
-    packet = ether / arp
-
-    result = srp(packet, timeout=2, verbose=0)[0]
+    Returns:
+        list of dicts: [{'ip':..., 'mac':...}]
+    """
 
     devices = []
 
+    arp_request = ARP(pdst=network)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    packet = broadcast / arp_request
+
+    result = srp(packet, timeout=2, verbose=0)[0]
+
     for sent, received in result:
-        devices.append({
+
+        device = {
             "ip": received.psrc,
             "mac": received.hwsrc
-        })
+        }
+
+        devices.append(device)
 
     return devices
