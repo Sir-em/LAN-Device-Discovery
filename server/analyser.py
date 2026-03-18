02b4419559a316059b9ebc7aae056cc9ab3af10e
Sir-em <meristiffany@gmail.com>
2026-03-18 22:09:14 +0530

    Update analyser.py

--- server/analyser.py
+++ server/analyser.py
@@ -1,77 +1,13 @@
-import time
-from collections import defaultdict
-from rules import evaluate_rules
-from ml_model import predict_anomaly
-from utils import calculate_risk_score
-
-devices = {}
-alerts = []
-device_history = {}
-
-TIME_WINDOW = 300  # 5 minutes
+AUTHORIZED_MACS = [
+    "A4:5E:60:22:11:FF",
+    "88:31:AA:CC:DD:22"
+]
 
 
-def update_device(log):
-    ip = log["ip"]
+def analyze_log(log):
+
     mac = log["mac"]
-    timestamp = log["timestamp"]
 
-    if ip not in devices:
-        devices[ip] = {
-            "ip": ip,
-            "mac": mac,
-            "first_seen": timestamp,
-            "last_seen": timestamp,
-            "join_count": 1,
-            "leave_count": 0,
-            "events": [timestamp],
-            "history_mac": set([mac])
-        }
-    else:
-        device = devices[ip]
-        device["last_seen"] = timestamp
-        device["join_count"] += 1
-        device["events"].append(timestamp)
-        device["history_mac"].add(mac)
+    if mac not in AUTHORIZED_MACS:
 
-
-def compute_features(device):
-    now = time.time()
-
-    recent_events = [t for t in device["events"] if now - t <= TIME_WINDOW]
-
-    return {
-        "session_duration": device["last_seen"] - device["first_seen"],
-        "join_frequency": device["join_count"] / max(1, (device["last_seen"] - device["first_seen"])),
-        "recent_activity": len(recent_events),
-        "is_new": (now - device["first_seen"]) < 60,
-        "ip_mac_mismatch": len(device["history_mac"]) > 1
-    }
-
-
-
-def analyze_device(ip, status):
-    import time
-
-    if ip not in device_history:
-        device_history[ip] = {
-            "first_seen": time.time(),
-            "events": 1
-        }
-    else:
-        device_history[ip]["events"] += 1
-
-    score = 0
-    rules = []
-
-    # New device
-    if device_history[ip]["events"] == 1:
-        score += 2
-        rules.append("NEW_DEVICE")
-
-    # Frequent reconnects
-    if device_history[ip]["events"] > 3:
-        score += 3
-        rules.append("HIGH_RECONNECT_RATE")
-
-    return score, rules
+        print("ALERT: Unauthorized device detected:", mac)
