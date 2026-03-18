b9f76c3d423a98f00b7855102c6c3a6148899ca6
Sir-em <meristiffany@gmail.com>
2026-03-18 22:10:06 +0530

    Update main_agent.py

--- agent/main_agent.py
+++ agent/main_agent.py
@@ -1,56 +1,33 @@
 import time
+
 from arp_scanner import scan_network
+from event_manager import EventManager
 from log_sender import send_log
 
-SCAN_INTERVAL = 10  # seconds
-known_devices = {}  # {ip: mac}
+NETWORK = "192.168.1.0/24"
 
-
-def create_log(ip, mac, status):
-    return {
-        "ip": ip,
-        "mac": mac,
-        "timestamp": time.time(),
-        "status": status  # "online" or "offline"
-    }
+SCAN_INTERVAL = 30   # seconds
 
 
 def main():
+
+    manager = EventManager("LAB-01")
+
     print("LAN Monitoring Agent Started")
 
-    global known_devices
-
     while True:
-        try:
-            current_devices = scan_network()
 
-            # Convert list → dict for easy comparison
-            current_dict = {d["ip"]: d["mac"] for d in current_devices}
+        devices = scan_network(NETWORK)
 
-            print("\nScanned Devices:", current_dict)
+        events = manager.process_scan(devices)
 
-            # ✅ Detect NEW devices (JOIN)
-            for ip, mac in current_dict.items():
-                if ip not in known_devices:
-                    print(f"[+] New Device: {ip} ({mac})")
-                    log = create_log(ip, mac, "online")
-                    send_log(log)
+        for event in events:
 
-            # ✅ Detect REMOVED devices (LEAVE)
-            for ip, mac in known_devices.items():
-                if ip not in current_dict:
-                    print(f"[-] Device Left: {ip} ({mac})")
-                    log = create_log(ip, mac, "offline")
-                    send_log(log)
+            print(event)
 
-            # 🔄 Update state
-            known_devices = current_dict
+            send_log(event)
 
-            time.sleep(SCAN_INTERVAL)
-
-        except Exception as e:
-            print("Error in agent:", e)
-            time.sleep(5)
+        time.sleep(SCAN_INTERVAL)
 
 
 if __name__ == "__main__":
